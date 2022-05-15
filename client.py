e19ada6fea6edb30b374bfe3a629ddfc469652d9
Ivan Zhou <ivan.zhouyq@gmail.com>
2022-05-15 16:41:16 -0700

    Remove http from host address

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from utils import get_master_address
 def get_request(url):
 	master_address = get_master_address()
 	proxies = {
-		'http': f'{master_address["host"]}:{master_address["port"]}',
+		'http': f'http://{master_address["host"]}:{master_address["port"]}',
 	}
 	return requests.get(url, proxies=proxies)
 
