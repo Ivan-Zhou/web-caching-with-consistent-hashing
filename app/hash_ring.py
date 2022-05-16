b3a4bd780290610add145858e6151545fe6469e4
Ivan Zhou <ivan.zhouyq@gmail.com>
2022-05-15 20:06:05 -0700

    Heartbeat in the cache server (#7)

--- app/hash_ring.py
+++ app/hash_ring.py
@@ -3,9 +3,18 @@ import time
 
 from .consistent_hashing import ConsistentHashing
 
+FLUSH_INTERVAL = 10  # seconds
+
+
 class HashRing:
-    def __init__(self, nodes=[], hash_fn=None, vnodes=100, weight_fn=None,
-                 flush_interval = 10000):
+    def __init__(
+        self,
+        nodes=[],
+        hash_fn=None,
+        vnodes=100,
+        weight_fn=None,
+        flush_interval=FLUSH_INTERVAL,
+    ):
         """
         Create a new hash ring.
 
@@ -23,26 +32,26 @@ class HashRing:
         self.vnodes = vnodes
         self.cons_hash = ConsistentHashing()
         self._configure_nodes(nodes)
-        self.heartbeat_interval = heartbeat_intval
         self.flush_interval = flush_interval
 
-    def handle_heartbeat(node_name):
+    def handle_heartbeat(self, node_name):
         """
-          Handle a heartbeat message. Update the lastHeartbeat time in the meta
-          data associated with node with "node_name". Do nothing if no such node
-          exists (e.g. the heartbeat message arrives too late due to network delays
-          and the node has already been deleted due to being inactive.
+        Handle a heartbeat message. Update the lastHeartbeat time in the meta
+        data associated with node with "node_name". Do nothing if no such node
+        exists (e.g. the heartbeat message arrives too late due to network delays
+        and the node has already been deleted due to being inactive.
 
-          Parameters
-          ----------
-          node_name : str
-              name identifying the node.
+        Parameters
+        ----------
+        node_name : str
+            name identifying the node.
         """
         node_meta = self.cons_hash.get_node_meta(node_name)
         if node_meta is not None:
             node_meta["lastHeartbeat"] = time.time()
         else:
             # Add new node to consistent caching
+            print(f"Add a new node to consistent caching: {node_name}")
             self.add_node(node_name)
 
     def flush(self):
@@ -52,6 +61,7 @@ class HashRing:
         Parameters
         ----------
         """
+        print(f"Flush the hash ring at {time.ctime()}")
         self.cons_hash.flush(time.time() - self.flush_interval)
 
     def _get(self, key, item):
--- proxy.py
+++ proxy.py
@@ -2,8 +2,11 @@ import socket
 import sys
 import signal
 import _thread
+import threading
 
 from utils import get_master_address
+from app import HashRing, FLUSH_INTERVAL
+from heartbeat import HEART_BEAT_INTERVAL
 
 MAX_CONN = 1
 RECV_SIZE = 4096
@@ -23,35 +26,36 @@ class Proxy:
             self.proxySocket.bind((master_address["host"], master_address["port"]))
 
             self.proxySocket.listen(MAX_CONN) # become a proxy socket
+            self.hash_ring = HashRing()
         except Exception as e:
-            print("Some error occured on init")
-            print(e)
+            print(f"Error occured on Proxy init: {e}")
             self.proxySocket.close()
             return
 
 
     def request_handler(self, clientSocket, clientAddr, clientData):
+        if clientData == "heartbeat":
+            print(f"Receive a heartbeat message from a Cache Server {clientAddr}")
+            self._handle_heartbeat(clientAddr)
+        else:
+            requestInfo = self.parse_request_info(clientAddr, clientData)
+            print(f"Sending request to origin server {requestInfo['server_url']}")
+            # create server socket (socket to talk to origin server)
+            serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serverSocket.connect((requestInfo["server_url"], requestInfo["server_port"]))
+            serverSocket.send(requestInfo["client_data"])
+            print("receiving reply from origin server")
 
-        # parse the get request
-        requestInfo = self.parse_request_info(clientAddr, clientData)
-        print("sending request to origin server")
-
-        # create server socket (socket to talk to origin server)
-        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serverSocket.connect((requestInfo["server_url"], requestInfo["server_port"]))
-        serverSocket.send(requestInfo["client_data"])
-        print("receiving reply from origin server")
-
-        # get reply for server socket (origin server)
-        reply = serverSocket.recv(RECV_SIZE)
-        print("sending reply to client server")
-        while len(reply):
-            clientSocket.send(reply)
+            # get reply for server socket (origin server)
             reply = serverSocket.recv(RECV_SIZE)
-        clientSocket.send(str.encode("\r\n\r\n"))
-        print("finished sending reply to client")
+            print("sending reply to client server")
+            while len(reply):
+                clientSocket.send(reply)
+                reply = serverSocket.recv(RECV_SIZE)
+            clientSocket.send(str.encode("\r\n\r\n"))
+            print("finished sending reply to client")
 
-        serverSocket.close()
+            serverSocket.close()
         clientSocket.close()
 
     '''
@@ -59,7 +63,6 @@ class Proxy:
     '''
     def parse_request_info(self, client_addr, client_data):
         try:
-
             lines = client_data.splitlines()
             while lines[len(lines)-1] == '':
                 lines.remove('')
@@ -105,7 +108,6 @@ class Proxy:
 
         except Exception as e:
             print(e)
-            print
             return None
 
     def service_requests(self):
@@ -113,13 +115,48 @@ class Proxy:
             try:
                 clientSocket, clientAddr = self.proxySocket.accept()
                 clientData = clientSocket.recv(RECV_SIZE)
-                print("recieved a request from ", clientAddr)
                 _thread.start_new_thread(self.request_handler, (clientSocket, clientAddr, str(clientData, encoding='utf-8', errors='ignore')))
             except KeyboardInterrupt:
                     clientSocket.close()
                     self.proxySocket.close()
                     break
 
+    def _handle_heartbeat(self, clientAddr):
+        # use the Host Address as the nodename
+        self.hash_ring.handle_heartbeat(node_name=clientAddr[0])
 
-proxy = Proxy()
-proxy.service_requests()
+    def get_node_name_for_hashkey(self, hash_key):
+        """
+          Get the nodename for a hash key.
+          Parameters
+          ----------
+          hash_key : str
+          Return
+          ----------
+          node_name : str
+        """
+        node_name = self.hash_ring.get_node(hash_key)
+        return node_name
+
+    def _flush(self):
+        """
+          Remove inactive nodes. Called every self.flush_interval
+          milliseconds.
+        """
+        self.hash_ring.flush()
+
+    def run(self):
+        """
+        Run scheduled tasks in thread to maintain cache servers
+        """
+        t1 = threading.Thread(target=self.service_requests)
+        t2 = threading.Timer(FLUSH_INTERVAL, self._flush)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+
+if __name__ == '__main__':
+    proxy = Proxy()
+    proxy.run()
