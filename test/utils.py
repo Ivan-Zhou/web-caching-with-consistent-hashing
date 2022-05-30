4ca17728606f4c824b4b1148b1f37a3684bce723
Yu Liu <yul231@stanford.edu>
2022-05-30 12:27:59 -0700

    Modify benchmark to user our own server

--- test/utils.py
+++ test/utils.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from PIL import Image
 from io import BytesIO
+import json
+ADDRESS_DATA_FILE = "../data.json"
 
 
+    
 
 def get_test_urls(n):
 	df = pd.read_csv("test/test_data.csv")[:n]
@@ -15,3 +18,15 @@ def load_image_content(content):
 
 def save_image(image, out_path="image.jpg"):
 	image.save(out_path, 'JPEG')
+
+def get_address_data():
+    with open(ADDRESS_DATA_FILE, "r") as f:
+        return json.load(f)
+
+def get_webserver_address():
+    address_data = get_address_data()
+    return address_data["webserver"]
+
+def get_section_num():
+    address_data = get_address_data()
+    return address_data["section_num"]
