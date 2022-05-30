601f9fe80a15e452f80ea896e3da2fe9f7e0d54b
Sharonlyu <64433031+Sharonlyu@users.noreply.github.com>
2022-05-30 13:00:19 -0700

    Integration test (#20)
    Co-authored-by: Shreya Ravi <sravi2@stanford.edu>
    Co-authored-by: Yu Liu <yul231@stanford.edu>

--- test/utils.py
+++ test/utils.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from PIL import Image
 from io import BytesIO
+import json
+DATA_FILE = "../data.json"
 
 
+    
 
 def get_test_urls(n):
 	df = pd.read_csv("test/test_data.csv")[:n]
@@ -15,3 +18,15 @@ def load_image_content(content):
 
 def save_image(image, out_path="image.jpg"):
 	image.save(out_path, 'JPEG')
+
+def load_data():
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+
+def get_webserver_address():
+    data = load_data()
+    return data["webserver"]
+
+def get_section_num():
+    data = load_data()
+    return data["section_num"]
