a14098c0fb6dcfdad8b1d1ffa81e0ffc5154eb24
Ivan Zhou <ivan.zhouyq@gmail.com>
2022-05-31 12:01:20 -0700

    Single hash table baseline (#21)
    * proxy forward requests
    
    * add retry after node removed
    
    * add single hashtable baseline
    
    * fix bugs in singleHashTable and add tests
    
    * refactor hash_utils
    
    * fix import issues
    
    * add single hash table flush/handle-heartbeat
    
    * single hashtable fixed with test cases
    
    * bug fixed
    
    Co-authored-by: Julius Zhang <juliuszh@myth66.stanford.edu>
    Co-authored-by: Julius Zhang <juliuszh@myth59.stanford.edu>
    Co-authored-by: Julius Zhang <juliuszh@myth56.stanford.edu>
    Co-authored-by: Julius Zhang <juliuszh@myth54.stanford.edu>

--- app/consistent_hashing.py
+++ app/consistent_hashing.py
@@ -42,5 +42,5 @@ class ConsistentHashing:
     def flush(self, time_stamp):
         for key in self._nodes:
             node_meta = self._nodes[key]
-            if node_meta["lastHeartbeat"] < time_stamp:
+            if node_meta["lastHeartbeat"] is None or node_meta["lastHeartbeat"] < time_stamp:
                 self.remove_node(node_meta["nodename"])
--- app/single_hash_table.py
+++ app/single_hash_table.py
@@ -1,24 +1,46 @@
 from .hash_utils import FLUSH_INTERVAL, md5_hash
+import time
 
 class singleHashTable:
     def __init__(self, hash_fn=md5_hash):
         self.hash_fn = hash_fn
-        self.nodes = []
+        self.nodes = {}
 
     def get_node(self, key):
-        return self.nodes[self.get_node_idx(key) % len(self.nodes)]
+        if not self.nodes:
+            return None
+        return list(self.nodes.values())[self.get_node_idx(key) % len(self.nodes)]
 
     def add_node(self, node):
-        self.nodes.append(node)
+        if node["nodename"] not in self.nodes:
+            self.nodes[node["nodename"]] = node
 
     def remove_node(self, nodename):
-        # Note this does not check if nodename is in self.nodes!
-        self.nodes.pop(self.get_node_idx(nodename))
-
-    def flush(self, time_stamp):
-        for node_meta in self.nodes:
-            if node_meta["lastHeartbeat"] < time_stamp:
-                self.remove_node(node_meta["nodename"])
+        if not self.nodes:
+            return
+        if nodename in self.nodes:
+            self.nodes.pop(nodename, None)
 
     def get_node_idx(self, key):
         return self.hash_fn(key) % len(self.nodes)
+
+    def handle_heartbeat(self, node_name):
+        """
+        Handle a heartbeat message. Update the lastHeartbeat time in the meta
+        data associated with node with "node_name". Do nothing if no such node
+        exists (e.g. the heartbeat message arrives too late due to network delays
+        and the node has already been deleted due to being inactive.
+
+        Parameters
+        ----------
+        node_name : str
+            name identifying the node.
+        """
+        if node_name in self.nodes:
+            self.nodes[node_name]["lastHeartbeat"] = time.time()
+
+    def flush(self, time_stamp):
+        for nodename in self.nodes.copy():
+            node_meta = self.nodes[nodename]
+            if node_meta["lastHeartbeat"] is None or node_meta["lastHeartbeat"] < time_stamp:
+                self.remove_node(node_meta["nodename"])
